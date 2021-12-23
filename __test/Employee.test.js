010c7c7ffe127bd5e6488321af57fbc94b992410
Mona Lisa <mekotigi@gmail.com>
2021-12-22 23:16:45 -0500

    add engineer thest

---
+++ __test/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require("../script/Employee");
+
+describe("Employee", () => {
+    it("Can instantiate Employee instance", () => {
+        const e = new Employee();
+        expect(typeof(e)).toBe("object");
+    });
+})
